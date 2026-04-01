9a5a99d19a658764888f30387dbcfd4253220fe2
Luan-etecvav <luanalvespadovani@gmail.com>
2026-04-01 14:50:18 -0300

    Add files via upload

---
+++ exercicios-php/exercicio5/index.php
@@ -0,0 +1,25 @@
+<!--
+Data: 20/03/2026
+Feito por: Luan Padovani
+Objetivo: Aprofundar os conhecimentos em HTML e PHP
+
+Exercício 5 - Somatório de Fatoriais
+Leia 5 números inteiros e mostre a soma dos fatoriais desses números.
+
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <form  action="script.php" method="post">
+    Digite 5 números separados por vírgula: 
+    <input type="text" name="nums" placeholder="Ex: 1,2,3,4,5" required>
+    <input type="submit" value="Somar Fatoriais">
+</form>
+</body>
+</html>
